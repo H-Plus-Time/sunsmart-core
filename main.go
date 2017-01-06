a6fc857efe06dc4ff92cc4ce0229b9a6e477889a
H-Plus-Time <nicholas.roberts.au@gmail.com>
2017-01-06 22:20:35 +1100

    Minor additions"

--- main.go
+++ main.go
@@ -6,12 +6,15 @@ import (
     "encoding/xml"
     "io/ioutil"
     "reflect"
+    "sync"
     // "time"
     // "github.com/robfig/cron"
 )
 
 const (
      serverKey = "YOUR-KEY"
+     MIN_THRESHOLD=0
+     MAX_THRESHOLD=16
 )
 
 type Location struct {
@@ -30,7 +33,36 @@ type Arpansa_Query struct {
 }
 // construct funcs for each interval
 
-func publishUVIndex(scheduled_time string, threshold float64) {
+func publish_worker(time_of_day int) {
+  resp, err := http.Get("http://www.arpansa.gov.au/uvindex/realtime/xml/uvvalues.xml")
+  if err != nil {
+    fmt.Println(err)
+  }
+  defer resp.Body.Close()
+
+  var stations Arpansa_Query;
+  body, err := ioutil.ReadAll(resp.Body)
+
+  if err != nil {
+    panic(err.Error())
+  }
+
+	xml.Unmarshal(body, &stations)
+  // do stuff with stations
+  for _,element := range stations.LocationList {
+    // element is the element from someSlice for where we are
+    fmt.Println(element)
+    for i := 0; i <= 16; i++ {
+        fmt.Println(i)
+        if element.Index >= float64(i) {
+          publishUVIndex(time_of_day, i, element)
+        }
+    }
+    // get firebase topic subscriptions
+  }
+}
+
+func publishUVIndex(time_of_day int, threshold int, data Location) {
     // data := map[string]string{
     //     "msg": fmt.Sprintf("Max UV: %f", max_uv),
     //     "sum": "Sunsmart UV Warning",
@@ -69,4 +101,20 @@ func main() {
   fmt.Println(string(body))
 	xml.Unmarshal(body, &q)
   fmt.Println(q)
+
+  messages := make(chan int)
+  var wg sync.WaitGroup
+  wg.Add(1)
+  go func() {
+    defer wg.Done()
+    publish_worker(0)
+    messages <- 1
+  }()
+
+  go func() {
+      for i := range messages {
+          fmt.Println(i)
+      }
+  }()
+  wg.Wait()
 }
